4e5b7567a7420bb69a74556e179962fa76c4f9f1
Leandro Galli <legalli1993@gmail.com>
2022-05-10 19:44:05 -0300

    criacao tests select query builder

---
+++ tests/LGA.Queries.Core.Tests/SelectQueryBuilderTest.cs
@@ -0,0 +1,33 @@
+﻿
+using LGA.Queries.Core.Builders;
+using Xunit;
+
+namespace LGA.Queries.Core.Tests
+{
+    public class SelectQueryBuilderTest
+    {
+
+        [Theory]
+        [InlineData(new string[] { "IdCliente", "Nome" }, "Cliente", "SELECT Cliente.IdCliente, Cliente.Nome\r\nFROM Cliente WITH(NOLOCK)\r\n")]
+        public void ShouldBuildSelectQuery(string[] fields, string table, string query)
+        {
+            var selectQueryBuilder = new SelectQueryBuilder(fields, table);
+
+            selectQueryBuilder.Build();
+
+            Assert.Equal(query, selectQueryBuilder.Query);
+        }
+
+        [Theory]
+        [InlineData(new string[] { "IdCliente", "Nome" }, "Cliente", "SELECT Cliente.IdCliente, Cliente.Nome\r\nFROM Cliente WITH(NOLOCK)\r\n")]
+        public void ShouldBuildSelectInnerJoinQuery(string[] fields, string table, string query)
+        {
+            var selectQueryBuilder = new SelectQueryBuilder(fields, table);
+
+            selectQueryBuilder.Build();
+
+            Assert.Equal(query, selectQueryBuilder.Query);
+        }
+
+    }
+}
